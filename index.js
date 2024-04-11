20915e3200711d0a101ef62ff07ed6051a2edab7
WhaleKimm <tmdals128551@gmail.com>
2024-04-11 23:57:39 +0900

    사진 추가, intro 페이지 틀 & 글자 넣기

---
+++ index.js
@@ -0,0 +1,24 @@
+// 맨 위로 이동 버튼
+let moveToTop = function () {
+  document.body.scrollIntoView({ behavior: "smooth" });
+};
+
+// 타이핑 효과
+const $txt = document.querySelector(".txt-title");
+const content = "안녕하세요.\n개발자 오승민입니다.";
+let contentIndex = 0;
+
+let typing = function () {
+  $txt.innerHTML += content[contentIndex];
+  contentIndex++;
+  if (content[contentIndex] === "\n") {
+    $txt.innerHTML += "<br />";
+    contentIndex++;
+  }
+  if (contentIndex > content.length) {
+    $txt.textContent = "";
+    contentIndex = 0;
+  }
+};
+
+setInterval(typing, 150);
