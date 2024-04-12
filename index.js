9e764431b7d42694dc4aa8ca452bb9545c2db1fa
WhaleKimm <tmdals128551@gmail.com>
2024-04-12 14:56:44 +0900

    취미, 성격 & 모달 기능 & 스크롤바 추가

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ const IMG_WIDTH = 329;
 const $btnPrev = document.querySelector(".btn-prev");
 const $btnNext = document.querySelector(".btn-next");
 const $slideImgs = document.querySelector(".slide-images");
-// JavaScript
 const $description = document.querySelector(".description"); // 설명 글씨 요소 선택
 
 let prev = function () {
@@ -81,3 +80,39 @@ let init = function () {
 };
 
 init();
+
+// 모달
+const $modalBg = document.getElementsByClassName("modal-background");
+const $btnOpen = document.getElementsByClassName("btn-open");
+const $btnClose = document.getElementsByClassName("btn-close");
+
+function modal(num) {
+  $btnOpen[num].addEventListener("click", () => {
+    $modalBg[num].style.display = "flex";
+    document.body.style.overflow = "hidden";
+  });
+  $btnClose[num].addEventListener("click", () => {
+    $modalBg[num].style.display = "none";
+    document.body.style.overflow = "unset";
+  });
+}
+
+for (let i = 0; i < $btnOpen.length; i++) {
+  modal(i);
+}
+
+// 스크롤바
+let scrollTop = 0;
+let bar = document.getElementsByClassName("bar-ing")[0];
+
+window.addEventListener(
+  "scroll",
+  () => {
+    scrollTop = document.documentElement.scrollTop;
+    let per = Math.ceil(
+      (scrollTop / (document.body.scrollHeight - window.outerHeight)) * 100
+    );
+    bar.style.width = per + "%";
+  },
+  false
+);
