88b2bee3f485dd8f682915e65d20d58380e00673
WhaleKimm <tmdals128551@gmail.com>
2024-04-13 21:22:55 +0900

    모바일 수정 완료

--- index.js
+++ index.js
@@ -4,24 +4,26 @@ let moveToTop = function () {
 };
 
 // 타이핑 효과
-const $txt = document.querySelector(".txt-title");
-const content = "안녕하세요.\n개발자 오승민입니다.";
-let contentIndex = 0;
+if (window.innerWidth > 768){
+  const $txt = document.querySelector(".txt-title");
+  const content = "안녕하세요.\n개발자 오승민입니다.";
+  let contentIndex = 0;
 
-let typing = function () {
-  $txt.innerHTML += content[contentIndex];
-  contentIndex++;
-  if (content[contentIndex] === "\n") {
-    $txt.innerHTML += "<br />";
+  let typing = function () {
+    $txt.innerHTML += content[contentIndex];
     contentIndex++;
-  }
-  if (contentIndex > content.length) {
-    $txt.textContent = "";
-    contentIndex = 0;
-  }
-};
+    if (content[contentIndex] === "\n") {
+      $txt.innerHTML += "<br />";
+      contentIndex++;
+    }
+    if (contentIndex > content.length) {
+      $txt.textContent = "";
+      contentIndex = 0;
+    }
+  };
 
-setInterval(typing, 150);
+  setInterval(typing, 150);
+};
 
 // 이미지 슬라이드
 let imgIndex = 0;
